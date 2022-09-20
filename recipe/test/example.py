8049c0066113f7b93c55566985bf92f3ce36551c
Allan Leal <allanmulin@gmail.com>
2022-09-20 15:02:47 +0200

    Fixed example.py after new API

--- recipe/test/example.py
+++ recipe/test/example.py
@@ -5,11 +5,11 @@ y = [xi**2 for xi in x]
 
 fig = Figure()
 
-fig.titleText("SQUARE FUNCTION")
+fig.title("SQUARE FUNCTION")
 
-fig.xaxisTitleText("x")
-fig.yaxisTitleText("y")
+fig.xaxisTitle("x")
+fig.yaxisTitle("y")
 
-fig.addScatter(x, y, "x**2")
+fig.drawLine(x, y, "x**2")
 
 fig.save("xsquared.pdf")
