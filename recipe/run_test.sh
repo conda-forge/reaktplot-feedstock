6ad2ddb3df869c2525eca645dddb6e86816fed85
Allan Leal <allan.leal@erdw.ethz.ch>
2023-06-08 19:55:51 +0200

    Disabling linux_ppc64le and linux_aarch64

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# Skip the rest of this test script if we are on cross-compilation
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-    exit 0
-fi
-
-# Skip the rest of this test script if we are on linux_aarch64
-if [[ "$target_platform" == "linux_aarch64" ]]; then
-    exit 0
-fi
-
-# Skip the rest of this test script if we are on linux_ppc64le
-if [[ "$target_platform" == "linux_ppc64le" ]]; then
-    exit 0
-fi
-
 # Execute the Python test application using reaktplot
 python test/example.py
 
